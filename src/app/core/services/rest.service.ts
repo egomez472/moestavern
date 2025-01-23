a6bd275d6775381a2824d5b1a80f019825be732e
Eric <eric.gomez.unq@gmail.com>
2025-01-23 15:25:59 -0300

    🧑‍💻 Servicio "rest" para metodos HTTP
    Se agrega un servicio para controlar los metodos HTTP de una forma mas canalizada

--- src/app/core/services/rest.service.ts
+++ src/app/core/services/rest.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  // Método GET
+  get<T>(url: string, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
+    return this.http.get<T>(url, { params, headers });
+  }
+
+  // Método POST
+  post<T>(url: string, body: any, headers?: HttpHeaders): Observable<T> {
+    return this.http.post<T>(url, body, { headers });
+  }
+
+  // Método PUT
+  put<T>(url: string, body: any, headers?: HttpHeaders): Observable<T> {
+    return this.http.put<T>(url, body, { headers });
+  }
+
+  // Método DELETE
+  delete<T>(url: string, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
+    return this.http.delete<T>(url, { params, headers });
+  }
 }
