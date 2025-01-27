78053c425fdd932fd3e0461b3d9f2e6e3baf0261
Eric <eric.gomez.unq@gmail.com>
2025-01-27 20:53:58 -0300

    📝 Documentando codigo
    Se documenta el uso del servicio getCocktailByName

--- src/app/core/services/cocktails/cocktails.service.ts
+++ src/app/core/services/cocktails/cocktails.service.ts
@@ -15,6 +15,22 @@ export class CocktailsService {
     private restSvc: RestService
   ) { }
 
+  /**
+ * Obtiene una lista de cócteles a partir del nombre proporcionado.
+ *
+ * @param cocktailName - El nombre del cóctel que se desea buscar.
+ *                      Debe ser una cadena no vacía.
+ * @returns Un Observable que emite un array de cócteles.
+ *          Si el nombre del cóctel está vacío o solo contiene espacios,
+ *          se emite un array vacío.
+ *
+ * @throws {Error} Si el nombre del cóctel es inválido, se registra un error en la consola.
+ *
+ * Este método realiza una solicitud HTTP a un servicio REST para obtener
+ * los cócteles que coinciden con el nombre proporcionado. Utiliza
+ * `HttpParams` para establecer los parámetros de la consulta y
+ * transforma la respuesta con el pipe map utilizando la función `processCocktailResponse`.
+ */
   getCocktailByName(cocktailName: string): Observable<Cocktail[]> {
 
     if (!cocktailName || cocktailName.trim() === '') {
